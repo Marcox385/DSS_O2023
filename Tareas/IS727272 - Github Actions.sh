9be29df188082ff603cf4d42db00e4e401d90d24
Marco Cordero <marcko_385@hotmail.com>
2023-10-02 12:59:22 -0600

    FINAL FIX LET IT BE PLEASE

--- Tareas/IS727272 - Github Actions.sh
+++
@@ -1,4 +0,0 @@
-echo "Hello World! at $( date +"%d/%M/%Y" )";
-echo "PR Author: $PR_AUTHOR";
-echo "Last commit: $(git show -s --format=%s)";
-echo -e "Commit diff:\n$( git fetch && git diff main..HEAD )\n";
