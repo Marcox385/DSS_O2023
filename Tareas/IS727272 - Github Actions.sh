731fa870683d892e4865ef4bded3f2c9a9aedbe2
Marco Cordero <marcko_385@hotmail.com>
2023-10-02 12:30:59 -0600

    Pairing

--- Tareas/IS727272 - Github Actions.sh
+++ Tareas/IS727272 - Github Actions.sh
@@ -1,4 +1,4 @@
-echo "Hello World! at ${{ date +"%d/%M/%Y" }}";
-echo "PR Author: ${{ github.event.pull_request.user.login }}"
-echo "Last commit: ${{ github.event.head_commit.message }}"
-echo -e "Commit diff: ${{ git diff origin/main HEAD }}\n"
+echo "Hello World! at $( date +"%d/%M/%Y" )";
+echo "PR Author: $PR_AUTHOR";
+echo "Last commit: $(git show -s --format=%s)";
+echo -e "Commit diff:\n$( git fetch && git diff main..HEAD )\n";
