9a4f9bdcc617bb136a8f9fc341e46bac847e4933
Marco Cordero <marcko_385@hotmail.com>
2023-10-02 12:28:31 -0600

    Fix inside yml

--- Tareas/IS727272 - Github Actions.sh
+++ Tareas/IS727272 - Github Actions.sh
@@ -1,4 +1,4 @@
 echo "Hello World! at $( date +"%d/%M/%Y" )";
 echo "PR Author: $PR_AUTHOR";
-echo "Last commit: $PR_COMMIT";
+echo "Last commit: $(git show -s --format=%s)";
 echo -e "Commit diff:\n$( git fetch && git diff main..HEAD )\n";
