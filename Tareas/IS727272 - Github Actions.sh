9123bb0c17e92c0ff2b37059120fdf5bfb0d715f
Marco Cordero <marcko_385@hotmail.com>
2023-10-02 10:47:50 -0600

    Yet another fix
    GH Action fix

--- Tareas/IS727272 - Github Actions.sh
+++ Tareas/IS727272 - Github Actions.sh
@@ -1,4 +1,4 @@
 echo "Hello World! at $( date +"%d/%M/%Y" )";
 echo "PR Author: $PR_AUTHOR";
 echo "Last commit: $PR_COMMIT";
-echo -e "Commit diff:\n$( git diff HEAD^ HEAD )\n";
+echo -e "Commit diff:\n$( git fetch && git diff main..HEAD )\n";
